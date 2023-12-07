9f8f76c9ece2cd6835c1deefafe9363ef2bb37be
EnrickMartins <102999688+EnrickMartins@users.noreply.github.com>
2023-12-07 09:57:37 -0300

    update file

---
+++ app/Model/Constants/DefaultInput.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model\Constants;
+
+class DefaultInput
+{
+  const MIN_AMOUNT = 0.01;
+  const MAX_AMOUNT = 9999999;
+  const AMOUNT = '20.00';
+  const FREQUENCY = 'week';
+  const MONTH = 'January';
+  const DAY = 1;
+  const YEAR = '2011';
+}
